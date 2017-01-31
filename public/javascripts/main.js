ad3de6b5555bea2a96ee16ed722c44eedb282d5f
Chris Stapler <cstapler@iastate.edu>
2017-01-30 19:05:26 -0600

    Changes socket url

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
    var width   = canvas.width;
    var height  = canvas.height;
    var username = sessionStorage.getItem("current-user");
-   var socket  = io.connect('https://fierce-anchorage-63287.herokuapp.com', {query: "username=" + username});
+   var socket  = io.connect('https://sketch-guess-6328.herokuapp.com', {query: "username=" + username});
    refreshLeaderboard();
 
    canvas.onmousedown = function(e){ mouse.click = true; };
